02788c82fe31759e4ef2c4c71873262051255f45
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2022-02-21 18:33:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import FlashCardsPage from "./pages/FlashCardsPage";
+
+function App() {
+  return <FlashCardsPage />
+}
+
+export default App;
---
+++ src/pages/FlashCardsPage.jsx
@@ -0,0 +1,6 @@
+export default function FlashCardsPage() {
+  return (
+      <>
+      </>
+  )
+}
